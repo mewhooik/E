56bc9a3af96af6749c05e937841ff63cbb3f1f48
mewhooik <sujalchavdaa@gmail.com>
2026-07-21 23:21:43 +0900

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ from pyrogram.types import Message
 from pyrogram.errors import FloodWait
 
 # ───────────────── CONFIG ─────────────────
-BOT_TOKEN = "8646009620:AAFnz0TBeN675UJX52GQq5rEXvsWa-RWfvI"
+BOT_TOKEN = "8975139816:AAFtWktzhBH0VxsLG4brQRAnK5OpoXxgzlQ"
 API_ID = 22370234
 API_HASH = "706badded011715ae115e5ab3bf83f87"
 
